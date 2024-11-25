e0d2db89e6f56bf649f5ec3f0b3290185ebf975e
Chen Xianwei <severuschen2000@gmail.com>
2024-11-25 09:54:52 +0800

    merge modify

--- myapp/merge/main.py
+++ myapp/merge/main.py
@@ -9,29 +9,18 @@ import time
 
 app.add_static_files('/images', './images')
 
-with ui.header(bordered=True, elevated=True).style('background-color: #3874c8') as header:
-    with ui.row(align_items='center').classes('content-center'):
-        ui.image('images/mindlab.png').props('width=120px height=80px').classes("bg-white")
-        ui.label('MIND LAB APP --- MODEL MERGE').classes("font-bold text-2xl text-white")
 
 def handle_drop(todo: TodoItem, location: str):
     ui.notify(f'"{todo.name}" is now in {location}')
 
-todos = ToDoList('Model Adjusting', on_change=todo_ui.refresh)
+todos = ToDoList('', on_change=todo_ui.refresh)
 
 def add_model_pool(todos: ToDoList):
-    card = ui.card().classes('w-[600px] items-stretch')
-    with card:
-        ui.label().bind_text_from(todos, 'title').classes('text-bold ml-1 text-2xl')
-        todo_ui(todos)
-    global model_pool
-    global model_merge
-    model_pool = dnd.column('Model Pool', on_drop=handle_drop)
-    model_merge = dnd.column('Models to be merged', on_drop=handle_drop)
-    with card:
-        add_input = ui.input('New Models').classes('mx-12').mark('new-item')
-        add_input.on('keydown.enter', lambda: todos.add(add_input.value, model_pool))
-        add_input.on('keydown.enter', lambda: add_input.set_value(''))
+    pass
+    # card = ui.card().classes(' items-stretch')
+    # with card:
+    #     ui.label().bind_text_from(todos, 'title').classes('text-bold ml-1 text-2xl')
+    #     todo_ui(todos)
 
 def add_drag(model_pool: dnd.column, model_merge: dnd.column):
     with model_pool:
@@ -66,30 +55,53 @@ async def compute():
     await asyncio.sleep(1)
     n.dismiss()
 
-with ui.column().classes('w-full mx-auto items-stretch'):
-    with ui.row().classes('w-full mx-auto content-center items-stretch'):
-        add_model_pool(todos)
-        # add_drag(model_pool, model_merge)
 
-    with ui.card().classes('w-full mx-auto items-stretch'):
-        with ui.column():
-            ui.label('Merge Options').classes('text-bold text-2xl')
-            with ui.row():
-                with ui.row(align_items='center'):
-                    ui.checkbox("architectural space").classes('text-lg flex-grow')
-                ui.space()
-                with ui.row(align_items='center'):
-                    # ui.label("Maximum number of iterations:").classes('text-lg flex-grow')
-                    ui.input("Maximum number of iterations:" , value=10).classes('text-lg flex-grow').props('clearable')
-                ui.space()
-                with ui.row(align_items='center'):
-                    # ui.label().classes('text-lg flex-grow')
-                    ui.input("Population size:", value=10).classes('text-lg flex-grow').props('clearable')
-                ui.space()
-                with ui.row(align_items='center'):
-                    ui.label("Select the EA algorithm:").classes('text-lg flex-grow')
-                    ui.select({1: 'One', 2: 'Two', 3: 'Three'}, value=1).classes('text-lg flex-grow')
-            # Create the UI
-        ui.button('Go Merging!', on_click=compute, icon='thumb_up')
+with ui.row().classes('w-full mx-auto items-stretch'):
+    # with ui.row().classes('mx-auto content-center items-stretch'):
+    with ui.column().classes(' mx-auto content-center items-stretch'):
+        add_model_pool(todos)
+        model_pool = dnd.column('Model Pool', on_drop=handle_drop)
+        model_merge = dnd.column('Models to be merged', on_drop=handle_drop)
+        todos.add('Llama', model_pool)
+        todos.add('QWen', model_pool)
+        todos.add('Llama', model_pool)
+        todos.add('QWen', model_pool)
+        todos.add('Llama', model_merge)
+        todos.add('QWen', model_merge)
+        # add_drag(model_pool, model_merge)
+    # with ui.row(align_items='center').classes('w-full mx-auto content-center items-stretch'):
+    with ui.column().classes('mx-auto content-center items-stretch'):
+        ui.label('').classes('h-[100px] text-bold text-2xl')
+        with ui.card().classes('w-[1100px] mx-auto items-stretch'):
+            with ui.column(align_items='center'):
+                with ui.column(align_items='center'):
+                    with ui.row(align_items='center').classes('content-center'):
+                        ui.image('images/mindlab.png').props('width=300px height=').classes("bg-white")
+                        ui.label('MINDLAB APP --- MODEL MERGE').classes("font-bold text-5xl text-black")
+                    ui.label('Merge Options').classes('text-bold text-2xl')
+                    with ui.row():
+                        with ui.column():
+                            with ui.row(align_items='center'):
+                                ui.checkbox("architectural space").classes('text-lg flex-grow')
+                            ui.space()
+                            with ui.row(align_items='center'):
+                                ui.label("Select the EA algorithm:").classes('text-lg flex-grow')
+                                ui.select({1: 'One', 2: 'Two', 3: 'Three'}, value=1).classes('text-lg flex-grow')
+                        ui.space()
+                        with ui.column():
+                            with ui.row(align_items='center'):
+                                # ui.label("Maximum number of iterations:").classes('text-lg flex-grow')
+                                ui.input("Maximum number of iterations:" , value=10).classes('text-lg flex-grow').props('clearable')
+                            ui.space()
+                            with ui.row(align_items='center'):
+                                # ui.label().classes('text-lg flex-grow')
+                                ui.input("Population size:", value=10).classes('text-lg flex-grow').props('clearable')
+                    # Create the UI
+                # ui.button('', icon='thumb_up').classes('w-1/2 h-[50px]')
+                with ui.row().classes('items-stretch'):
+                    ui.button('Go Merging!', on_click=compute, icon='published_with_changes').classes('w-[500px] h-[80px] text-xl flex-grow')
+                    ui.button('Go Chatting', icon='diversity_3').classes('w-[500px] h-[80px] text-xl flex-grow')
+                    ui.button('Go Translation', icon='translate').classes('w-[500px] h-[80px] text-xl flex-grow')
+                    ui.button('Go Summary', icon='grading').classes('w-[500px] h-[80px] text-xl flex-grow')
 
 ui.run(native=False, )
